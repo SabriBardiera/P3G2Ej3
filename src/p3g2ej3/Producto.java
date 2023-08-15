b61f5344b85b191b1832bfde2274b7935c21fecf
SabriBardiera <sabrinabardiera@hotmail.com>
2023-08-15 19:13:45 -0300

    Initial commit

---
+++ src/p3g2ej3/Producto.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p3g2ej3;
+
+/**
+ *
+ * @author sabri
+ */
+public class Producto {
+    
+    private String categoria, nombre;
+    private double precio;
+
+    public Producto(String categoria, String nombre, double precio) {
+        this.categoria = categoria;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    
+    
+}
